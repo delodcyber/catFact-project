0245e5fbbf01492c3dbcd4b18fd9dd53c98c5fcd
delodcyber <Truebasisoflife@yahoo.com>
2026-06-13 21:27:17 +0100

    feat: change root directory to /src

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { resolve, dirname } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: 'index.html',
-                exploreCats: 'src/explore-cat/explore-cat.html',
-                // add any other pages here
-            }
-        }
-    }
+  root: "src/",
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://catfact-project.onrender.com/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        "explore-cats": resolve(__dirname, "src/explore-cats/explore-cat.html"),
+        breeds: resolve(__dirname, "src/breeds/breeds.html"),
+        favorites: resolve(__dirname,"src/favorite/favorite.html"),
+      },
+    },
+  },
 });
