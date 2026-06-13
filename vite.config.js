3794bd4451b4e6beb25b131fdee175d5c38f993b
delodcyber <Truebasisoflife@yahoo.com>
2026-06-13 23:51:15 +0100

    feat: refactor spartials

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,11 @@
 import { resolve, dirname } from "path";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
-import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Plugin to copy partials into dist
-function copyPartials() {
-  return {
-    name: "copy-partials",
-    generateBundle() {
-      const files = ["head.html", "header.html", "footer.html"];
-
-      for (const file of files) {
-        const filePath = resolve(__dirname, "src/public/partials", file);
-        const content = fs.readFileSync(filePath, "utf-8");
-
-        this.emitFile({
-          type: "asset",
-          fileName: `partials/${file}`,
-          source: content,
-        });
-      }
-    },
-  };
-}
-
 export default defineConfig({
-  plugins: [copyPartials()],
-
   root: "src/",
 
   server: {
@@ -47,9 +23,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        "explore-cats": resolve(__dirname, "src/explore-cats/explore-cat.html"),
-        breeds: resolve(__dirname, "src/breeds/breeds.html"),
-        favorites: resolve(__dirname, "src/favorite/favorite.html")
+        "explore-cats": resolve(__dirname, "src/pages/explore-cat.html"),
+        breeds: resolve(__dirname, "src/pages/breeds.html"),
+        favorites: resolve(__dirname, "src/pages/favorite.html")
       },
     },
   },
