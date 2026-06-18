af6dcca36f4e453c968728c26fe2765c34e4abd9
delodcyber <Truebasisoflife@yahoo.com>
2026-06-18 10:13:36 +0100

    feat: add new pages for About, Breeds, and Explore Cats with corresponding content

--- vite.config.js
+++ vite.config.js
@@ -23,9 +23,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        "explore-cats": resolve(__dirname, "src/explore-cats/explore-cat.html"),
-        breeds: resolve(__dirname, "src/breeds/breeds.html"),
-        favorites: resolve(__dirname, "src/about/about.html")
+        "explore-cats": resolve(__dirname, "src/pages/explore-cat.html"),
+        breeds: resolve(__dirname, "src/pages/breeds.html"),
+        favorites: resolve(__dirname, "src/pages/about.html")
       },
     },
   },
